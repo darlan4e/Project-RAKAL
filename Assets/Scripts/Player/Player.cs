c46a9a15c7e5d605e7b66aefe090b0986ed4ac3b
darlan <klenk260805@gmail.com>
2025-04-18 11:44:03 +0300

    UPD.02
    Added plugin InputSystem for editing and using custom ActionMap
    remaked player input in Player.cs by using new ActionMap

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float dashSpeed;
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
     private GameObject _cursor;
+    public float health;
+    private InputManager _inputActions;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _inputActions = new InputManager();
         _animator = GetComponent<Animator>();
         _cursor = GameObject.FindWithTag("Cursor");
     }
@@ -19,35 +24,54 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("жопа");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Health();
         Move();
+        //Dash();
         Animation();
         LocalScaleRotate();
     }
 
-    void Move()
+    //void Dash(){}
+    
+    private void OnEnable()
     {
-        float hor = Input.GetAxis("Debug Horizontal");
-        float ver = Input.GetAxis("Debug Vertical");
-
-        _rigidbody2D.velocity = new Vector2(hor, ver).normalized * speed;
+        _inputActions.Enable();
     }
 
-    void Animation()
+    private void OnDisable()
     {
-        if (_rigidbody2D.velocity == Vector2.zero) 
-        {
-            _animator.SetBool(Animator.StringToHash("Walk"),false);
-        }
-        else 
-        {
-            _animator.SetBool(Animator.StringToHash("Walk"),true);
-        }
+        _inputActions.Disable();
+    }
+    
+    void Move()
+    {
+        //Получаем ввод из кастомного действия Movement
+        Vector2 moveInput = _inputActions.Player.Movement.ReadValue<Vector2>();
+        
+        // Применяем движение
+        _rigidbody2D.velocity = moveInput.normalized * speed;
+    }
+    
+    void Health()
+    {
+
+    }
+
+    private void Animation()
+    {
+        // Получаем текущий ввод (даже если он нулевой)
+        Vector2 moveInput = _inputActions.Player.Movement.ReadValue<Vector2>();
+    
+        // Проверяем, есть ли ввод (более точная проверка чем сравнение velocity)
+        bool isMoving = moveInput.sqrMagnitude > 0.01f;
+    
+        _animator.SetBool(Animator.StringToHash("Walk"), isMoving);
     }
 
     void LocalScaleRotate()
